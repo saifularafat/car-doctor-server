df9cb8afdfd366a7f7980958e0f8326eebf2a8db
saifularafat <saifularafat.info@gmail.com>
2023-05-12 01:49:43 +0600

    server added

--- index.js
+++ index.js
@@ -8,9 +8,6 @@ const port = process.env.PORT || 4000;
 app.use(cors())
 app.use(express.json())
 
-//VhjZ30vgTpeQomwI
-//car-doctor-user
-console.log();
 
 
 const { MongoClient, ServerApiVersion } = require('mongodb');
